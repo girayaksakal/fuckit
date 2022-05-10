4ba713feaa3bd779d35fb1595273f3b8a3f1aabd
girayaksakal <102882253+girayaksakal@users.noreply.github.com>
2022-05-10 18:59:40 +0300

    legal

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@
 import "./css/main.css";
 
 // Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+console.log("Created by Giray Aksakal.");
+console.log("For business inquiries:");
+console.log("girayaksakal@icloud.com");
